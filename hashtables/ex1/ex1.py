6ac321141e0da69312786c353d8ed25684011f51
mikepadiernos <mike.padiernos@gmail.com>
2020-09-11 22:05:25 -0400

    [BUILD] completion / ex1
    - finished code for ex1
    - test passed for ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,20 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+
+    # loop through i as index in the array
+    # using the value of length
+    for i in range(length):
+        # see if an index is in the dict
+        if weights[i] not in cache:
+            # add limit minus weight to the dict as the key
+            # with the index as value
+            cache[limit - weights[i]] = i
+        else:
+            # arrange the indexes where it returns
+            # the higher number
+            if cache[weights[i]] > i:
+                return cache[weights[i]], i
+            else:
+                return i, cache[weights[i]]
 
     return None
