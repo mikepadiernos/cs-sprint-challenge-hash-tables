3c01ad3756a5bc37d658d53d4f73601f20b3940a
mikepadiernos <mike.padiernos@gmail.com>
2020-11-08 17:04:26 -0500

    [FEATURE] completion / ex3
    - completed code for exercise 3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,27 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # print(arrays)
+    cache = {}
+    result = []
+
+    # loop through the arrays
+    for array in arrays:
+        # print(array)
+
+        # loop through an individual array for integers
+        for integer in array:
+
+            # determine if an integer is in the cache
+            if integer in cache:
+                cache[integer] += 1
+            else:
+                cache[integer] = 1
+
+    # loop through the cache for the integers
+    for i in list(cache.items()):
+        # determine if an item is equal to the length of the array
+        if i[1] == len(arrays):
+            # append the item to the result
+            result.append(i[0])
 
     return result
 
